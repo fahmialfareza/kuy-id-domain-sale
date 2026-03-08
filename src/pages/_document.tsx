e66c3e073ad47b576a0b084b46ae7b944d65d2ef
Fahmi Alfareza <signer_aero0l@icloud.com>
2026-03-09 05:01:43 +0700

    feat: add Cloudflare Web Analytics script to document head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Cloudflare Web Analytics */}
+        <script
+          defer
+          src="https://static.cloudflareinsights.com/beacon.min.js"
+          data-cf-beacon='{"token": "0fd44994b4f045188c4df4091265f8a6"}'
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
